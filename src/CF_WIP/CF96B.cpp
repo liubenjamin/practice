6ed7e9733ff3741eecc589e925f5f5f3766ffdc3
Benjamin Liu <liu.benjamin@gmail.com>
2020-09-01 21:14:17 -0700

    Add CF300B

---
+++ src/CF_WIP/CF96B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll ans;
+ll n;
+
+void gen(long long i, int four, int seven) {
+    if(i >= n && seven == four) ans = std::min(ans, i);
+    if(i > n * 100) return;
+    gen(i * 10 + 4, four + 1, seven);
+    gen(i * 10 + 7, four, seven + 1);
+}
+
+int main() {
+    cin >> n;
+    gen(0, 0, 0);
+    cout << ans << endl;
+    return 0;
+}
